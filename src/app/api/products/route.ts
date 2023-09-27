2b73c2aa5f565b8606893e88c6676156aa42f654
JULIÁN ABELLÁN ABAD <Julian.Abellan@alu.uclm.es>
2023-09-27 09:04:00 +0200

    prueba de commit

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,9 +1,8 @@
 import { ProductsResponse, getProducts } from '@/lib/handlers';
 import { NextRequest, NextResponse } from 'next/server';
 export async function GET(
-request: NextRequest
+  request: NextRequest
 ): Promise<NextResponse<ProductsResponse>> {
-const products = await getProducts();
-return NextResponse.json(products);
+  const products = await getProducts();
+  return NextResponse.json(products);
 }
-
