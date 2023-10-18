ca33ec6cf0ec6a63b1598a7d3459087f0c98062b
RAÚL ARROYO TORRIJOS <raulat99@gmail.com>
2023-10-18 20:25:06 +0200

    Añadidos autorizacion endpoints

--- src/app/api/users/[userId]/orders/[orderId]/route.ts
+++ src/app/api/users/[userId]/orders/[orderId]/route.ts
@@ -1,6 +1,9 @@
 import { Types } from 'mongoose';
 import { NextRequest, NextResponse } from 'next/server';
 import { getOrder, getUser, OrderResponse } from '@/lib/handlers';
+import { Session } from 'next-auth';
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '@/lib/authOptions';
 
 export async function GET(
   request: NextRequest,
@@ -10,8 +13,18 @@ export async function GET(
     params: { userId:string, orderId: string };
   }
 ): Promise<OrderResponse | {}> {
+
+  const session: Session | null = await getServerSession(authOptions);
+
+  if (!session?.user) {
+    return NextResponse.json({}, { status: 401 });
+  }
+
   if (!Types.ObjectId.isValid(params.userId)) {
     return NextResponse.json({}, { status: 400 });
+  }
+  if (session.user._id !== params.userId) {
+    return NextResponse.json({}, { status: 403 });
   }
 
   const user = await getUser(params.userId);
--- src/lib/authOptions.ts
+++ src/lib/authOptions.ts
@@ -4,6 +4,7 @@ import CredentialsProvider from 'next-auth/providers/credentials';
 import Users from '@/models/User';
 import bcrypt from 'bcrypt';
 
+
 export const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
@@ -21,26 +22,28 @@ export const authOptions: NextAuthOptions = {
       },
       async authorize(credentials, req) {
         await connect();
+        console.log("Aqui");
 
         if(!credentials?.email || !credentials?.password){
+            console.log("El email o password no existen");
             return null;
         }
         
         const user = await Users.findOne({email: credentials.email})
         
         if(user === null){
+          console.log("El user es nulo");
             return null;
-        }
-
-        if (user.password !== credentials.password){
-          return null;
         }
 
         const match = await bcrypt.compare(credentials.password, user.password);
 
         if(!match){
+          console.log("Las contraseñas no coinciden");
           return null;
         }
+
+        console.log("User id: " + user._id.toString());
 
         return { _id: user._id.toString() } as User;
       },
