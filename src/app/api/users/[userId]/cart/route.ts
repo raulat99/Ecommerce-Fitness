bba599b0c5e7ea2be2ce8597b3aa12a28ee5252a
JULIÁN ABELLÁN ABAD <Julian.Abellan@alu.uclm.es>
2023-10-18 20:01:48 +0200

    cambios

--- src/app/api/users/[userId]/cart/route.ts
+++ src/app/api/users/[userId]/cart/route.ts
@@ -1,5 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { CartResponse, getCart } from '@/lib/handlers';
+import { Session } from 'next-auth';
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '@/lib/authOptions';
+
 
 export async function GET(
   request: NextRequest,
@@ -9,14 +13,17 @@ export async function GET(
     params: { userId: string };
   }
 ): Promise<NextResponse<CartResponse> | {}> {
-  
-    const cart = await getCart(params.userId);
-  
+
+  const session: Session | null = await getServerSession(authOptions);
+  if (!session?.user) {
+    return NextResponse.json({}, { status: 401 });
+  }
+
+  const cart = await getCart(params.userId);
+
   if (params.userId === null) {
     return NextResponse.json({}, { status: 400 });
   }
 
-  return NextResponse.json(
-    cart
-  );
+  return NextResponse.json(cart);
 }
