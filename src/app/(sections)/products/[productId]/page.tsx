49db3114edf3f1bf77f1968b7b13271df2616ea3
JULIÁN ABELLÁN ABAD <Julian.Abellan@alu.uclm.es>
2023-11-08 20:30:42 +0100

    empiezo productid

--- src/app/(sections)/products/[productId]/page.tsx
+++ src/app/(sections)/products/[productId]/page.tsx
@@ -3,7 +3,7 @@ import { notFound } from 'next/navigation';
 import { getProduct } from '@/lib/handlers';
 
 export default async function Product({
-  params, 
+  params,
 }: {
   params: { productId: string };
 }) {
@@ -18,12 +18,38 @@ export default async function Product({
   }
 
   return (
-    <div className='flex flex-col'>
-      <h3 className='pb-4 text-3xl font-bold text-gray-900 sm:pb-6 lg:pb-8'>
-        {product.name}
-      </h3>
-      {product.description && <p>{product.description}</p>}
-      {/* Here you should show the details of the product. */}
+    <div className='flex flex-col sm:flex-row'>
+      <div className='max-w-md'>
+        <img
+          src={product.img}
+          alt={product.name}
+          className='w-full rounded-lg object-cover object-center group-hover:opacity-75'
+        />
+
+        <div className='my-5 ml-3'>
+          {product.price && (
+            <p
+              style={{
+                fontSize: '56px',
+                fontFamily: 'Roboto, sans-serif'
+              }}
+            >
+              {product.price + ' €'}
+            </p>
+          )}
+        </div>
+      </div>
+
+      <div className='w-full sm:w-1/2'>
+        <h3 className='pb-4 text-3xl font-bold text-gray-900 sm:pb-6 lg:pb-8'>
+          {product.name}
+        </h3>
+
+        <div>
+          <p className='font-bold'>Product Details</p>
+          {product.description && <p>{product.description}</p>}
+        </div>
+      </div>
     </div>
   );
 }
