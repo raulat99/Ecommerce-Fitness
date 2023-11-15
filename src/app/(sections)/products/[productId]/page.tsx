03093adcab34ad3b4ac03178780d102ec7e1bdab
JULIÁN ABELLÁN ABAD <Julian.Abellan@alu.uclm.es>
2023-11-15 17:40:43 +0100

    avance en 2/3

--- src/app/(sections)/products/[productId]/page.tsx
+++ src/app/(sections)/products/[productId]/page.tsx
@@ -19,7 +19,7 @@ export default async function Product({
 
   return (
     <div className='flex flex-col sm:flex-row'>
-      <div className='max-w-md'>
+      <div className='max-w-sm'>
         <img
           src={product.img}
           alt={product.name}
@@ -31,16 +31,30 @@ export default async function Product({
             <p
               style={{
                 fontSize: '56px',
-                fontFamily: 'Roboto, sans-serif'
+                fontFamily: 'Roboto, sans-serif',
               }}
             >
               {product.price + ' €'}
             </p>
           )}
         </div>
+
+        <div className='custom-number-input h-10 w-32 ml-5 my-5'>
+          <div className='relative mt-1 flex h-10 w-full flex-row rounded-lg bg-transparent'>
+            <button className=' h-full w-20 cursor-pointer rounded-l bg-gray-300 text-gray-600 outline-none hover:bg-gray-400 hover:text-gray-700'>
+              <span className='m-auto text-2xl font-thin'>−</span>
+            </button>
+
+            <span className="outline-none focus:outline-none text-center w-full bg-gray-300 font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700  outline-none">0</span>
+
+            <button className='h-full w-20 cursor-pointer rounded-r bg-gray-300 text-gray-600 hover:bg-gray-400 hover:text-gray-700'>
+              <span className='m-auto text-2xl font-thin'>+</span>
+            </button>
+          </div>
+        </div>
       </div>
 
-      <div className='w-full sm:w-1/2'>
+      <div className='ml-5 w-full sm:w-1/2'>
         <h3 className='pb-4 text-3xl font-bold text-gray-900 sm:pb-6 lg:pb-8'>
           {product.name}
         </h3>
